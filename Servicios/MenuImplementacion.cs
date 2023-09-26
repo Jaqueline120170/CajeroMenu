985829e7d4efb60f8a8d06b2ed026e5714dd12c7
Jaqueline <csi23-jromres@colegioaltair.net>
2023-09-26 14:14:25 +0200

    Agregar archivos de proyecto.

---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroMenu.Servicios
+{
+    /// <summary>
+    /// Clase que implementa la interfaz de menú
+    /// 25092023 - jrt
+    /// 
+    /// </summary>
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void mostrarMensajeBienvenida()
+        {
+            string mensaje = "Banco CSI1"; 
+                Console.WriteLine(mensaje);
+        }
+        public int mostrarMenuYSeleccion()
+        {
+            int opcionIntroducida;
+
+            Console.WriteLine("##########################");
+            Console.WriteLine("0. Cerra aplicacion");
+            Console.WriteLine("1. Darse de alta");
+            Console.WriteLine("2. Ingresar dinero");
+            Console.WriteLine("3. Sacar dinero");
+            Console.WriteLine("4. Transferir dinero");
+            Console.WriteLine("5. Historial de operaciones");
+            Console.WriteLine("################################");
+            Console.WriteLine("Selecciona una opcion: ");
+
+            opcionIntroducida = Console.ReadKey(true).KeyChar-('0');
+
+            return opcionIntroducida;
+        }
+    }
+}
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroMenu.Servicios
+{
+    /// <summary>
+    /// Interfaz con la relación de métodos con la funcionalidad menú
+    /// 25092023 - jrt
+    /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Método que muestra mensaje de bienvenida al cajero. 
+        /// 25092023 - jrt
+        /// </summary>
+       public void mostrarMensajeBienvenida();
+
+        /// <summary>
+        /// Método que muestra el menú y recoje la selección del usuario
+        /// 26092023 - jrt
+        /// </summary>
+        /// <returns>un entero con la opción seleccionada</returns>
+        public int mostrarMenuYSeleccion();
+    }
+}
